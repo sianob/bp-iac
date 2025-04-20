fd1491225d0735d59cda4cc3458fa317be6c8d62
sian <sianob79@gmail.com>
2025-04-20 22:15:03 +0100

    Sian/ingress (#8)
    * install ingress nginx as part of iac
    
    * add k8s provider and retrieve public ingress ip
    
    * need to login to cluster
    
    * rollback ingress

--- aks-staging/providers.tf
+++ aks-staging/providers.tf
@@ -4,14 +4,6 @@ terraform {
         source = "hashicorp/azurerm"
         version = "4.26.0"
     }
-     helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.10"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.22"
-    }
   }
   backend "azurerm" {
       resource_group_name  = "tfstate"
@@ -23,20 +15,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-}
-
-provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
-  }
 }
