d3e6e58a177bfcc7e81b0f5e49c040f3a08e5a19
salob <sianob79@gmail.com>
2025-04-20 20:42:18 +0100

    install ingress nginx as part of iac

--- aks-staging/output.tf
+++ aks-staging/output.tf
@@ -14,3 +14,12 @@ output "kubeconfig" {
   value     = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
 }
+
+# Output the public IP
+# Might not be available immediately
+# run 'terraform output ingress_nginx_public_ip'
+
+output "ingress_nginx_public_ip" {
+  value       = data.kubernetes_service.ingress_nginx.status[0].load_balancer[0].ingress[0].ip
+  description = "Public IP address of the Ingress NGINX load balancer"
+}
