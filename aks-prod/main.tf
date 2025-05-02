9982b5d1f7a527791437230e527fd0ac1a5aee59
salob <sianob79@gmail.com>
2025-05-03 00:03:59 +0100

    update availability zones

--- aks-prod/main.tf
+++ aks-prod/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     node_count          = var.system_node_count
     vm_size             = "Standard_DS2_v2"
     type                = "VirtualMachineScaleSets"
-    zones  = [1, 2, 3]
+    zones  = [2]
     auto_scaling_enabled = true
     min_count           = 1
     max_count           = 3
